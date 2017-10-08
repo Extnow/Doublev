d3b14b817a571ac97b55c95860eb1df24e8af022
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2017-10-08 23:19:33 +0300

    refactoring1

--- main.js
+++ main.js
@@ -1,35 +1,29 @@
 jQuery(document).ready(function(){
 
-  $('#tab-mat').click(function(){
-    $('#materials').show("500");
-    $('#final_product').hide();
-    $('#technology').hide();
-    $('.section__products').css('background', 'url(img/pr1_bg.png) no-repeat 50%, #e0dede');
-  })
+  function showProduct (tab, cat1, cat2, cat3, img, background) {
+    $(tab).click(function(){
+      $(cat1).show("500");
+      $(cat2).hide();
+      $(cat3).hide();
+      $('.section__products').css('background', 'url(img/' + img + ') no-repeat 50%,' + background );
+    })
+  };
 
-  $('#tab-prod').click(function(){
-    $('#materials').hide();
-    $('#final_product').show("500");
-    $('#technology').hide();
-    $('.section__products').css('background', 'url(img/pr2_bg.png) no-repeat 50%, #ddddde');
-  })
+  function changeActiveTab () {
+    $(".tab").click(function(e) {
+      e.preventDefault();
+      $(".tab").removeClass('tab-active');
+      $(this).addClass('tab-active');
+    })
+  }
 
-  $('#tab-tech').click(function(){
-    $('#materials').hide();
-    $('#final_product').hide();
-    $('#technology').show("500");
-    $('.section__products').css('background', 'url(img/pr3_bg.png) no-repeat 50%, #e8e8e8');
-  })
+  showProduct('#tab-mat', '#materials', '#final_product', '#technology', 'pr1_bg.png', '#e0dede');
+  showProduct('#tab-prod', '#final_product', '#materials', '#technology', 'pr2_bg.png', '#ddddde');
+  showProduct('#tab-tech', '#technology', '#final_product', '#materials', 'pr3_bg.png', '#e8e8e8');
+  changeActiveTab();
 
-  $(".tab").click(function(e) {
-    e.preventDefault();
-    $(".tab").removeClass('tab-active');
-    $(this).addClass('tab-active');
-  })
-
-  $('#catalog').hover(function(e) {
-    $('.menu').css('visibility', 'visible');
-  });
+  // $('#catalog').click(function(e) {
+  //   $('.menu').css('visibility', 'visible');
+  // });
   
-
 });
